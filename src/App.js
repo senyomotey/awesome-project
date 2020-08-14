9aaca5371ad0a17703bee8930b63f7505d0c0060
senyo-motey <47481875+senyo-motey@users.noreply.github.com>
2020-08-14 12:01:47 +0000

    unun

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import axios from 'axios'
 
 class App extends React.Component {
   componentDidMount() {
+    var formData = new FormData();
+    formData.set('product_unique_id', '574ed359ce2e18.82356240');
+    //formData.append('product_unique_id', '574ed359ce2e18.82356240');
+
     axios({
       method: 'post',
-      url: 'https://okukus.com/api_call/get_books.php',
-    }).then(response => {
+      url: 'http://localhost:80/okukus/public_html/api_call/get_book.php',
+      data: formData,
+      headers: {'Content-Type': 'multipart/form-data'}
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error)
+    });
+
+    axios({
+      method: 'post',
+      url: 'http://localhost:80/okukus/public_html/api_call/get_book.php?product_unique_id=574ed359ce2e18.82356240:',
+      data: {
+      }
+    })
+    .then(response => {
       console.log(response);
     })
     .catch(error => {
