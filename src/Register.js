3b04c7d63b2d796bddb5797fc56801349cb73b49
senyo-motey <47481875+senyo-motey@users.noreply.github.com>
2020-08-14 17:18:48 +0000

    jgg

--- src/Register.js
+++ src/Register.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 
 class App extends React.Component {
   componentDidMount() {
-    /*
     var formData = new FormData();
     formData.set('email', 'moteyrakim@gmail.com');
     formData.set('firstname', 'rakim');
@@ -24,26 +23,6 @@ class App extends React.Component {
     .catch(error => {
       console.log(error)
     });
-    */
-
-    var formData = new FormData();
-    formData.set('email', 'moteyrakim@gmail.com');
-    formData.set('password', '1234asdf');
-    //formData.append('product_unique_id', '574ed359ce2e18.82356240');
-
-    axios({
-      method: 'post',
-      url: 'http://localhost:80/okukus/public_html/api_call/user_login.php',
-      data: formData,
-      headers: {'Content-Type': 'multipart/form-data'}
-    })
-    .then(response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.log(error)
-    });
-    
   }
   render() {
     return <h1>my Component has Mounted, Check the browser 'console' </h1>;
